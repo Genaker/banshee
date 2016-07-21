c615cca7f45f908c6d3e9b33f2268a5d74d3542b
egors <egors@compandandsave.com>
2016-07-21 12:23:58 -0700

    code format fix

--- code/Router.php
+++ code/Router.php
@@ -1,49 +1,47 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Michael
  * Date: 7/20/2016
  * Time: 5:10 PM
  */
-class Router {
+class Router
+{
     protected $request;
     public $view;
-    
-    protected $map = 
+
+    protected $map =
         [
-         'safsdfsaf'=>'error',
-        'v'=>'video',
-        'q'=>'list',
+            'safsdfsaf' => 'error',
+            'v' => 'video',
+            'q' => 'list',
         ];
 
     protected $map_exclude =
         [
-            'safsdfsaf'=>0,
-            'v'=>0,
-            'q'=>0,
+            'safsdfsaf' => 0,
+            'v' => 0,
+            'q' => 0,
         ];
 
-    
+
     public function __construct($request)
     {
         $this->request = $request;
     }
-    
+
     public function getViewName()
     {
         if (@$this->map[$this->request->pathArray[0]]) {
-        $this->view = $this->map[$this->request->pathArray[0]];
-        }
-        else {
+            $this->view = $this->map[$this->request->pathArray[0]];
+        } else {
             $this->view = 'index';
         }
-        if (@$this->map_exclude[$this->request->pathArray[0]] == true)
-        {
+        if (@$this->map_exclude[$this->request->pathArray[0]] == true) {
             unset($this->request->pathArray[0]);
             $this->request->parseUrl();
         }
-     return $this->view;
+        return $this->view;
     }
-
-
 }
